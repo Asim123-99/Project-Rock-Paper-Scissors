3a3309c27e872d9affd69e06990a68c6653ed16f
Asim Nasar <Asim_n123@hotmail.com>
2023-09-01 13:06:15 +0100

    Inputting code so we can declare a winner

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("Hello World!");
+
+const options = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  const choice = options [Math.floor(Math.random() * options.length)];
+  return choice;
+}
+
+
+
+
+function checkWinner(playerSelection, computerSelection) {
+  if(playerSelection == computerSelection) {
+    return "Tie";
+  }
+  else if (
+   (playerSelection == "rock" && computerSelection == "scissors") ||
+   (playerSelection == "paper" && computerSelection == "rock") ||
+   (playerSelection == "scissors" && computerSelection == "paper") 
+    
+  ){
+  return "Player";
+  }
+
+  else {
+    return "Computer";
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+  
+}
+
+getComputerChoice();
